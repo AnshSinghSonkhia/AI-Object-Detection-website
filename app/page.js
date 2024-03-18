cb7acfec0ee43a4bf28f6df53975a475d78b944a
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2024-03-18 22:43:42 +0530

    updated: font-item-height

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center min-h-screen p-8">
       <div className="flex flex-col items-center">
-        <h1 className="text-3xl font-black tracking-tight text-center  gradient-title md:text-6xl lg:text-8xl md:px-6">AI Burglar Detector</h1>
-        <p className="pt-8 text-sm font-bold tracking-tighter text-center md:text-xl gradient-title">
+        <h1 className="text-3xl font-black tracking-tight text-center h-28 gradient-title md:text-6xl lg:text-8xl md:px-6">AI Burglar Detector</h1>
+        <p className="pt-2 text-sm font-bold tracking-tighter text-center md:text-xl gradient-title">
           Object detection using AI to detect burglar & raise alarm.
         </p>
         <p className="pt-0.5 text-xs font-bold tracking-tighter text-center md:text-xl gradient-title">
