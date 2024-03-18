8265df8daaba4493bee7fca5c37ada2ce22dffc6
Ansh Singh Sonkhia <110414565+AnshSinghSonkhia@users.noreply.github.com>
2024-03-18 22:36:40 +0530

    updated font

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,27 @@
-import { Inter } from "next/font/google";
+//import { Inter } from "next/font/google";
+
+import { DM_Sans } from 'next/font/google';
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+
+const dmSans = DM_Sans({
+  subsets: ['latin'], // Specify desired character subsets
+  weights: [100, 300, 400, 500, 700, 900], // Include the specific font weights you need
+  display: 'swap', // Ensure smooth font loading with 'swap'
+});
+
+//const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Thief Detection AI Alarm",
-  description: "Object detection using AI to detect thief and raise alarm. Created by Ansh Singh Sonkhia",
+  title: "AI Burglar Detector",
+  description: "Object detection using AI to detect burglar & raise alarm. Developed by Ansh Singh Sonkhia",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning className={inter.className}>{children}</body>
+      <body suppressHydrationWarning className={dmSans.className}>{children}</body>
+      {/* <body suppressHydrationWarning className={inter.className}>{children}</body> */}
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -5,9 +5,12 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center min-h-screen p-8">
       <div className="flex flex-col items-center">
-        <h1 className="text-3xl font-black tracking-tight text-center gradient-title md:text-6xl lg:text-8xl md:px-6">Thief Detection AI Alarm</h1>
-        <p className="pt-2 text-sm font-bold tracking-tighter text-center md:text-xl gradient-title">
-          Object detection using AI to detect thief and raise alarm. Developed by Ansh Singh Sonkhia
+        <h1 className="text-3xl font-black tracking-tight text-center  gradient-title md:text-6xl lg:text-8xl md:px-6">AI Burglar Detector</h1>
+        <p className="pt-8 text-sm font-bold tracking-tighter text-center md:text-xl gradient-title">
+          Object detection using AI to detect burglar & raise alarm.
+        </p>
+        <p className="pt-0.5 text-xs font-bold tracking-tighter text-center md:text-xl gradient-title">
+          Developed by Ansh Singh Sonkhia 
         </p>
       </div>
       <div className="flex flex-col items-center">
